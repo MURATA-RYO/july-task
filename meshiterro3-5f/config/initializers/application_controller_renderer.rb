9e3c179e222b86a6d32c3f53da9e13e1ce5a6b9d
MURATA-RYO <scincoidae@gmail.com>
2021-07-19 13:59:48 +0900

    [finish]3-5f

---
+++ meshiterro3-5f/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '878eaa03e97964c898aa217ed9e8e78eb396335930d5958870acd0f876d00ed5ea25ba2b48d71722b6db781a0e8a8e71d694b739b823f26c4491ba3c8af17e0e'
